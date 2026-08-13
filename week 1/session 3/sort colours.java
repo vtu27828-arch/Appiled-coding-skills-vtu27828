e89c9231ca2706475d3eb62ee03d8ee78b3ef2cc
vtu27828-arch <vtu27828@veltech.edu.in>
2026-08-13 09:58:57 +0530

    Create sort colours.java

---
+++ week 1/session 3/sort colours.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                // Swap low and mid
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+
+                low++;
+                mid++;
+            } 
+            else if (nums[mid] == 1) {
+                mid++;
+            } 
+            else {
+                // Swap mid and high
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+
+                high--;
+            }
+        }
+    }
+}
+
+Input
+nums =
+[2,0,2,1,1,0]
+Output
+[0,0,1,1,2,2]
