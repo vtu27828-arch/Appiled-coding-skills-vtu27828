91818fd727fb9cd3125acf42bcfc89bfdd7f9a50
vtu27828-arch <vtu27828@veltech.edu.in>
2026-08-13 10:02:19 +0530

    Create first unique characters in a string.java

---
+++ week 1/session 4/first unique characters in a string.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+
+        // Count frequency of each character
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        // Find the first character with frequency 1
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
+
+Input
+s =
+"leetcode"
+Output
+0
