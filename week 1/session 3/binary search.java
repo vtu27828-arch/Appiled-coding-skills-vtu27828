2766091beadb442d64b4d6cc03fadfb2103be682
vtu27828-arch <vtu27828@veltech.edu.in>
2026-08-13 09:57:44 +0530

    Add binary search implementation in Java
    Implemented binary search algorithm to find the target in a sorted array.

---
+++ week 1/session 3/binary search.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
+Input
+nums =
+[-1,0,3,5,9,12]
+target =
+9
+Output
+4
