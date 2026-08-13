5087f2df674fbb66bcf40e1c4032327a249cb9da
vtu27828-arch <vtu27828@veltech.edu.in>
2026-08-13 09:49:53 +0530

    Create squares of a sorted array.java

---
+++ week 1/session 1/squares of a sorted array.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int left = 0;
+        int right = n - 1;
+        int index = n - 1;
+
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if (leftSquare > rightSquare) {
+                result[index] = leftSquare;
+                left++;
+            } else {
+                result[index] = rightSquare;
+                right--;
+            }
+
+            index--;
+        }
+
+        return result;
+    }
+}
+
+Input
+nums =
+[-4,-1,0,3,10]
+Output
+[0,1,9,16,100]
