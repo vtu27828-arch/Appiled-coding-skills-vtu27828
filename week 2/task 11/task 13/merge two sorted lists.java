a4ce64387844a9bd5cf534177ab590a62eec63a7
vtu27828-arch <vtu27828@veltech.edu.in>
2026-08-20 09:33:11 +0530

    Create merge two sorted lists.java

---
+++ week 2/task 11/task 13/merge two sorted lists.java
@@ -0,0 +1,36 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            current = current.next;
+        }
+
+        // Add remaining nodes
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
+}
+Input
+list1 =
+[1,2,4]
+list2 =
+[1,3,4]
+Output
+[1,1,2,3,4,4]
