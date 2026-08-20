5e82fd44daf23b5a44b1b6cf1053225e5898805b
vtu27828-arch <vtu27828@veltech.edu.in>
2026-08-20 09:26:41 +0530

    Create middle of linked list.java

---
+++ week 2/task 11/middle of linked list.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
+
+Input
+head =
+[1,2,3,4,5]
+Output
+[3,4,5]
