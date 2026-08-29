4388379df5d8e6ef0e61dd508315bb6d7f601942
vtu27828-arch <vtu27828@veltech.edu.in>
2026-08-29 09:14:44 +0530

    Add Binary Tree Right Side View solution
    Implement a solution to return the right side view of a binary tree using breadth-first search.

---
+++ week 4/task 32/Binary Tree Right Side View.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode current = queue.poll();
+
+                // Last node of the current level
+                if (i == size - 1) {
+                    result.add(current.val);
+                }
+
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
+Input
+root =
+[1,2,3,null,5,null,4]
+Output
+[1,3,4]
